ae6ceca39ca90f501a6617677b57d1c2c36359f2
AluMati <AluMati@PC10>
2025-03-28 13:54:03 +0100

    imnpletar resta y multiplicacion

--- Calculadora/src/Calculadora/Calculadora.java
+++ Calculadora/src/Calculadora/Calculadora.java
@@ -4,7 +4,16 @@ public class Calculadora {
 	public int sumar(int a, int b) {
 		return a + b;
 	}
+	public int restar(int a, int b) {
+		 return a - b;
+		}
+		public int multiplicar(int a, int b) {
+		 return a * b;
+		}
 	public static void main(String[] args) {
 		Calculadora calc = new Calculadora();
 		System.out.println("Resultado de la suma: " +
-		calc.sumar(5, 3));}}
+		calc.sumar(5, 3));
+		}
+	
+	}
